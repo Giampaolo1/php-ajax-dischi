7e1a6217cd04f716592a221fce0837ce3ec67d4a
Giampaolo1 <giampaolo.r@outlook.com>
2019-12-11 16:58:19 +0100

    Inizio Ver2

--- ver2/index.php
+++ ver2/index.php
@@ -23,7 +23,7 @@ album per artista -->
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>JP php-ajax Dischi 1</title>
+    <title>JP php-ajax Dischi 2</title>
 
     <?php
       include "data.php";
@@ -31,26 +31,7 @@ album per artista -->
 
   </head>
   <body>
-    <?php
-        foreach ($database as $album) { ?>
-      <h2>
-        <?php echo $album["title"];?>
-      </h2>
-      <h3>
-        <?php echo $album["author"];?>
-      </h3>
-      <h3>
-        <?php echo $album["year"];?>
-      </h3>
-      <div>
-        <img src=<?php echo $album["poster"];?>>
-        <?php echo "<br>";?>
-        <?php echo "<br>";?>
-        <?php echo "-------------";;?>
-      </div>
-    <?php } ?>
-
-    <img src="">
-
+    <!-- JS: MY SCRIPT -->
+    <script src="script.js" charset="utf-8"></script>
   </body>
 </html>
