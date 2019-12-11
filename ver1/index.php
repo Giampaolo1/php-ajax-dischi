e011c478f28e99109e6b95b7e88a4306ae9d97de
Giampaolo1 <giampaolo.r@outlook.com>
2019-12-11 16:44:34 +0100

    V1 ok

---
+++ ver1/index.php
@@ -0,0 +1,54 @@
+<!-- Stampare a schermo una decina di dischi musicali
+(vedi screenshot) in due modi diversi:
+
+VER 1:
+
+- Solo con l'utilizzo di PHP, che stampa
+direttamente i dischi in pagina: al caricamento
+della pagina ci saranno tutti i dischi.
+
+VER 2:
+
+- Attraverso l'utilizzo di AJAX: al caricamento
+della pagina ajax chiederà attraverso una
+chiamata i dischi a php e li stamperà
+attraverso handlebars.
+
+Utilizzare: Html, Sass, JS, jQuery, handlebars, Php
+Opzionale:
+- Attraverso un'altra chiamata ajax, filtrare gli
+album per artista -->
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>JP php-ajax Dischi 1</title>
+
+    <?php
+      include "data.php";
+    ?>
+
+  </head>
+  <body>
+    <?php
+        foreach ($database as $album) { ?>
+      <h2>
+        <?php echo $album["title"];?>
+      </h2>
+      <h3>
+        <?php echo $album["author"];?>
+      </h3>
+      <h3>
+        <?php echo $album["year"];?>
+      </h3>
+      <h3>
+        <?php echo $album["poster"];?>
+        <?php echo "<br>";?>
+        <?php echo "<br>";?>
+        <?php echo "-------------";;?>
+      </h3>
+    <?php } ?>
+
+  </body>
+</html>
